2bfb1e855955afd42a9dc8ad65725cb17143bc63
Ismoil <ismoilzokirjanov191@gmail.com>
2022-10-07 01:26:57 +0500

    h1

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns =[
     path('',views.index),
+    path('medicine/', views.medicine)
+    
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,"dashboard.html")  
+
+def medicine(request):
+    return render(request,"medicine.html")
+    
